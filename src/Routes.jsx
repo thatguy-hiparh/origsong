99db1fc5f0ef983b06be35d77b8e21b50c293d09
VG <hiparh@yahoo.com>
2025-10-29 13:45:32 +0100

    Update import paths in Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
-// Add your imports here
-import Login from "pages/login";
-import Register from "pages/register";
-import AccountSettings from "pages/account-settings";
-import SongResearchResults from "pages/song-research-results";
-import ResearchDashboard from "pages/research-dashboard";
-import NotFound from "pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
+
+import Login from "./pages/login";
+import Register from "./pages/register";
+import AccountSettings from "./pages/account-settings";
+import SongResearchResults from "./pages/song-research-results";
+import ResearchDashboard from "./pages/research-dashboard";
+import NotFound from "./pages/NotFound.jsx";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <ErrorBoundary>
-      <ScrollToTop />
-      <RouterRoutes>
-        {/* Define your routes here */}
-        <Route path="/" element={<ResearchDashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/account-settings" element={<AccountSettings />} />
-        <Route path="/song-research-results" element={<SongResearchResults />} />
-        <Route path="/research-dashboard" element={<ResearchDashboard />} />
-        <Route path="*" element={<NotFound />} />
-      </RouterRoutes>
+        <ScrollToTop />
+        <RouterRoutes>
+          {/* Define your routes here */}
+          <Route path="/" element={<ResearchDashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/account-settings" element={<AccountSettings />} />
+          <Route path="/song-research-results" element={<SongResearchResults />} />
+          <Route path="/research-dashboard" element={<ResearchDashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </RouterRoutes>
       </ErrorBoundary>
     </BrowserRouter>
   );
